f724b0014f3cbb893916f757624f1ac363c86f0a
Sanskruti88joshi <138274859+Sanskruti88joshi@users.noreply.github.com>
2023-07-24 18:39:21 +0530

    Add files via upload

---
+++ array 11.cpp
@@ -0,0 +1,37 @@
+//aceept a program to accept n city names & accept a city to search.
+//(using function with flag).
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[100][100],city[50];
+	int n,i;
+	void search(char s1[100][100],char city[50],int n);
+	printf("enter limit=");
+	scanf("%d",&n);
+	printf("enter n city names=");
+	for(i=0;i<n;i++)
+	{
+		scanf("%s",&s1[i]);
+	}
+	printf("enter city name to search=");
+	scanf("%s",&city);
+	search(s1,city,n);
+}
+void search(char s1[100][100],char city[50],int n)
+{         
+	int i,flag=0;
+	for(i=0;i<n;i++)
+	{
+		if(strcmp(s1[i],city)==0)
+		{
+		
+			flag=1;
+			break;
+		}
+	}
+	if(flag==1)
+	printf("city found");
+	else 
+	printf("city is not found");
+}
